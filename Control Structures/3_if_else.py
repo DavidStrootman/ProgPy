580498109b75de9ac7d5cbfa951d69f15e40fb1d
DavidStrootman <david.strootman@gmail.com>
2018-09-05 16:59:25 +0200

    Added Control Structures 2 and 3

---
+++ Control Structures/3_if_else.py
@@ -0,0 +1,24 @@
+def get_bool(prompt):
+    while True:
+        try:
+            return {"ja": True, "nee": False}[input(prompt).lower()]
+        except ValueError:
+            print("Voer aub ja of nee in!")
+
+
+def get_age():
+    while True:
+        try:
+            return int(input('Geef je leeftijd op'))
+        except ValueError:
+            print('Geen correcte leeftijd opgegeven')
+
+
+age = get_age()
+
+dutchPassport = get_bool('nederlands paspoort?')
+
+if 18 <= age and dutchPassport:
+    print('Gefeliciteerd, je mag stemmen!')
+else:
+    print('Je mag helaas niet stemmen.')
