3efcd4e55d381fc245b1bee604402c40083c9a1c
DavidStrootman <david.strootman@gmail.com>
2018-09-06 10:08:21 +0200

    Added control structures 6

--- Control Structures/5_For, If and numbers.py
+++ Control Structures/5_For, If and numbers.py
@@ -1,4 +1,5 @@
 numbers = [2, 3, 4, 5, 20]
+
 for number in numbers:
     if number % 2 == 0:
         print(number)
---
+++ Control Structures/6_For, If and vowels.py
@@ -0,0 +1,4 @@
+string = 'Guido van Rossum heeft programmeertaal Python bedacht.'
+for letter in string:
+    if letter in 'aeiou':
+        print(letter)
