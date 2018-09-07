7ee557061cc58d505ea74ae0203ffbc49a9d9011
David Strootman <david.strootman@gmail.com>
2018-09-07 16:27:40 +0200

    Added close methods to files in strings 2 and 3

--- Strings, Text Data, and File IO/2_Files lezen.py
+++ Strings, Text Data, and File IO/2_Files lezen.py
@@ -3,3 +3,4 @@ with open('kaartnummers.txt', 'r') as file:
         number = line[:5]
         name = line[7:-1]
         print(name + ' heeft kaarnummer: ' + number)
+    file.close()
--- Strings, Text Data, and File IO/3_Files lezen.py
+++ Strings, Text Data, and File IO/3_Files lezen.py
@@ -7,6 +7,7 @@ with open('kaartnummers.txt', 'r') as file:
         if int(line[:6]) > highest_number:
             highest_number = int(line[:6])
             highest_line = line_number
+    file.close()
 
 print('Dit bestand heeft ' + str(i) + ' regels')
 print('Het grootste kaartnummer is: ' + str(highest_number) + ' en dat staat op regel ' + str(highest_line))
